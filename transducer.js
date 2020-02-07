d2aceba23db562acebdc7788c162cc5064dd8d02
trondeh80 <trond.erling@gmail.com>
2020-02-07 16:44:00 +0100

    Change some naming and comments

--- transducer.js
+++ transducer.js
@@ -1,20 +1,23 @@
-const arrayConcat = (memo, item) => memo.concat([item]);
+const compose = (...fns) => stepFn => 
+        fns.reduceRight((memo, item) => item(memo), stepFn);
 
-const compose = (...fns) => stepFn => fns.reduceRight((memo, item) => item(memo), stepFn);
+const map = mapFn => stepFn => 
+    (memo, item) => stepFn(memo, mapFn(item));
 
-const map = mapFn => stepFn => (memo, item) => stepFn(memo, mapFn(item));
-const filter = predicatFn => stepFn => (memo, item) => predicatFn(item) ? stepFn(memo, item) : memo;
+const filter = predicatFn => stepFn => 
+    (memo, item) => predicatFn(item) ? stepFn(memo, item) : memo;
 
 const isEven = (num) => num % 2 === 0;
 const doubleThat = (num) => num * 2;
 
-const evenFlowTransformer = compose(
+const evenDoubleTransformer = compose(
     filter(isEven),
     map(doubleThat)
 );
 
-const transformer = evenFlowTransformer(arrayConcat); // Pearl Jam
+const arrayConcat = (memo, item) => memo.concat([item]); // our "stepFn"
+const transformer = evenDoubleTransformer(arrayConcat);
 
-const list = [1,2,3,4,5,6,7,8,9].reduce(transformer, []);
+const list = [1,2,3,4,5,6,7,8,9].reduce(transformer, []); // [ 4, 8, 12, 16 ]
 
 console.log(list);
